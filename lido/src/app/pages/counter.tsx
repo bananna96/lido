8ed669ef7c60ec392b7adeb3b51b82cd040da85c
Anna Chiara <anna.laves@web.de>
2023-06-24 19:12:39 +0200

    [master] fixed counting

--- lido/src/app/pages/counter.tsx
+++ lido/src/app/pages/counter.tsx
@@ -1,6 +1,14 @@
-import { Stack, Paper, Button, TextField, InputAdornment } from "@mui/material";
+import {
+	Stack,
+	Paper,
+	Button,
+	TextField,
+	InputAdornment,
+	CircularProgress,
+	Box,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import employeeForm from "../components/employeeForm";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -13,10 +21,37 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Counter() {
 	const [totals, setTotals] = useState({ totalEuro: 0, totalHours: 0 });
-	const [items, setItems] = useState([{ id: 0, name: "", time: 0 }]);
+	const [items, setItems] = useState([
+		{ id: 0, name: "", time: 0, money: 0 },
+	]);
+	const [allMoneyGivenAndRest, setAllMoneyGivenAndRest] = useState({
+		all: 0,
+		rest: 0,
+	});
+
+	const [loaded, setLoaded] = useState(false);
+
+	// This will run one time after the component mounts
+	useEffect(() => {
+		const onPageLoad = () => {
+			setLoaded(true);
+		};
+
+		// Check if the page has already loaded
+		if (document.readyState === "complete") {
+			onPageLoad();
+		} else {
+			window.addEventListener("load", onPageLoad);
+			// Remove the event listener when component unmounts
+			return () => window.removeEventListener("load", onPageLoad);
+		}
+	}, []);
 
 	const addItem = function () {
-		setItems([...items, { id: items.length, name: "", time: 0 }]);
+		setItems([
+			...items,
+			{ id: items.length, name: "", time: 0, money: 0 },
+		]);
 	};
 	const updateEmpState = (id: number, field: string, event: any) => {
 		// TODO: shorter / less code?
@@ -68,52 +103,99 @@ export default function Counter() {
 			</div>
 		);
 	};
+
+	const splitMoney = (allMoneyEmp: number, totalE: number) => {
+		let diff = allMoneyEmp - totalE;
+		console.log(diff);
+	};
+
+	const count = (totalE: any, totalH: any) => {
+		let itemsSpeicher = [...items];
+		const moneyPerHour = totalE / totalH;
+		const rounded = Number(moneyPerHour.toFixed(1));
+		itemsSpeicher.forEach(item => {
+			item.money = Number((rounded * item.time).toFixed(1));
+		});
+
+		let allesVonEinzeln = 0;
+		itemsSpeicher.forEach(
+			it => (allesVonEinzeln = allesVonEinzeln + it.money)
+		);
+		if (allesVonEinzeln > totals.totalEuro) {
+			count(totalE - (allesVonEinzeln % totalE), totalH);
+		} else {
+			setItems(itemsSpeicher);
+			setAllMoneyGivenAndRest({
+				all: allesVonEinzeln,
+				rest: totals.totalEuro - allesVonEinzeln,
+			});
+		}
+	};
+
 	return (
 		<>
-			<h1>MOIN</h1>
-			<div>
-				<TextField
-					label="Total €"
-					type="number"
-					onChange={e => {
-						updateTotals("money", e);
-					}}
-					InputProps={{
-						startAdornment: (
-							<InputAdornment position="start">
-								€
-							</InputAdornment>
-						),
-					}}
-				/>
-			</div>
-			<Stack spacing={4}>
-				{items.map((formItem, index) => {
-					return (
-						<div key={index}>
-							{getEmployeeForm(formItem.id)}
-						</div>
-					);
-				})}
-				<Button onClick={addItem} variant="outlined">
-					ADD
-				</Button>
-				<Button onClick={addItem} variant="outlined">
-					Count
-				</Button>
-				<div>Total hours: {`${totals.totalHours}`}</div>
+			{!loaded ? (
+				<Box sx={{ display: "flex" }}>
+					<CircularProgress />
+				</Box>
+			) : (
 				<div>
-					( {`${items.length}`} ) Employees:{" "}
-					{items.map(i => {
-						return (
-							<div key={"name-" + i.id}>
-								<span>{i.name}</span>
-								<br />
-							</div>
-						);
-					})}
+					<h1>MOIN</h1>
+					<TextField
+						label="Total €"
+						type="number"
+						onChange={e => {
+							updateTotals("money", e);
+						}}
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									€
+								</InputAdornment>
+							),
+						}}
+					/>
+					<Stack spacing={4}>
+						{items.map((formItem, index) => {
+							return (
+								<div key={index}>
+									{getEmployeeForm(formItem.id)}
+								</div>
+							);
+						})}
+						<Button onClick={addItem} variant="outlined">
+							ADD
+						</Button>
+						<Button
+							onClick={() =>
+								count(
+									totals.totalEuro,
+									totals.totalHours
+								)
+							}
+							variant="outlined">
+							Count
+						</Button>
+						<div>Total hours: {`${totals.totalHours}`}</div>
+						<div>
+							Total Money splitted:{" "}
+							{`${allMoneyGivenAndRest.all}`}
+						</div>
+						<div>Rest: {`${allMoneyGivenAndRest.rest}`}</div>
+						<div>
+							( {`${items.length}`} ) Employees:{" "}
+							{items.map(i => {
+								return (
+									<div key={"name-" + i.id}>
+										<span>{i.name}</span>
+										<br />
+									</div>
+								);
+							})}
+						</div>
+					</Stack>
 				</div>
-			</Stack>
+			)}
 		</>
 	);
 }
